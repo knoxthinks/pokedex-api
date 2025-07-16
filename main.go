9d7b0f107b736d6c2aae6ae5f8f118dbeb9527bc
knox <knox_01@DESKTOP-6R7UQS5.localdomain>
2025-07-16 16:39:31 -0400

    bootdev solution, function a different file

--- main.go
+++ main.go
@@ -1,23 +1,27 @@
 package main
 
-import ("fmt" 
-		"bufio"
-		"os"
-		"strings"
-)
+// import ("fmt" 
+// 		"bufio"
+// 		"os"
+// 		"strings"
+// )
 
-func main(){
-	s := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		s.Scan()
-		line := s.Text()
-		words := strings.Fields(line)
-		if len(words) == 0 {
-			continue
-		}
-		firstWord := words[0]
-		lower := strings.ToLower(firstWord)
-		fmt.Printf("Your command was: %s\n", lower)
-	}
+// func main(){
+// 	s := bufio.NewScanner(os.Stdin)
+// 	for {
+// 		fmt.Print("Pokedex > ")
+// 		s.Scan()
+// 		line := s.Text()
+// 		words := strings.Fields(line)
+// 		if len(words) == 0 {
+// 			continue
+// 		}
+// 		firstWord := words[0]
+// 		lower := strings.ToLower(firstWord)
+// 		fmt.Printf("Your command was: %s\n", lower)
+// 	}
+// }
+
+func main() {
+	startRepl()
 }
