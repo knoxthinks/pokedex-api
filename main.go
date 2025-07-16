bb5c55d17608544f738d88b9b5b57de4f148c88f
knox <knox_01@DESKTOP-6R7UQS5.localdomain>
2025-07-16 16:27:01 -0400

    repl part, add input/response

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import ("fmt" 
+		"bufio"
+		"os"
+		"strings"
+)
 
 func main(){
-	fmt.Println("Hello, World!")
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		s.Scan()
+		line := s.Text()
+		words := strings.Fields(line)
+		if len(words) == 0 {
+			continue
+		}
+		firstWord := words[0]
+		lower := strings.ToLower(firstWord)
+		fmt.Printf("Your command was: %s\n", lower)
+	}
 }
-
-
-//TODO - github
