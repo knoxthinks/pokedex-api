1765dcd54d9ec9d84dbf34594a9a4f462cc7fdb2
knox <knox_01@DESKTOP-6R7UQS5.localdomain>
2025-07-21 16:19:07 -0400

    seer stone, change url to github

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"time"
+
+	"github.com/knoxthinks/pokedex-api/internal/pokeapi"
+)
 
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
