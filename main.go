1ceff6b2a28f0b920cf96cfde0ec0f7f3833d8a1
knox <knox_01@DESKTOP-6R7UQS5.localdomain>
2025-07-21 15:22:58 -0400

    clear comments from main.go

--- main.go
+++ main.go
@@ -1,28 +1,11 @@
 package main
 
-// import ("fmt" 
-// 		"bufio"
-// 		"os"
-// 		"strings"
-// )
-
-// func main(){
-// 	s := bufio.NewScanner(os.Stdin)
-// 	for {
-// 		fmt.Print("Pokedex > ")
-// 		s.Scan()
-// 		line := s.Text()
-// 		words := strings.Fields(line)
-// 		if len(words) == 0 {
-// 			continue
-// 		}
-// 		firstWord := words[0]
-// 		lower := strings.ToLower(firstWord)
-// 		fmt.Printf("Your command was: %s\n", lower)
-// 	}
-// }
+import "err"
 
 func main() {
-	//remove logic that prints first word back to user
-	// startRepl()
+}
+
+
+func commandExit() error {
+
 }
